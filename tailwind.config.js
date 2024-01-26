63fccac60aae6e0ff077ec7d5ae2e6c87262b9ea
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-01-25 19:32:46 -0800

    add content path to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './*.html', // Adjust the path to where your HTML files are
+    './*.js',     // Adjust the path to where your JavaScript files are
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
