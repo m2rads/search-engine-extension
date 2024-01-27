3d070491347a218a05aaea34c0121cd20c884b05
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-01-27 14:28:58 -0800

    fix typos and bugs=

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {action: "search", searchTerm: searchTerm}, function(response) {
                 if (response && !response.found) {
-                    console.log("Search term not found on this page.");
+                    alert("Search term not found on this page.");
                 }
             });
         });
