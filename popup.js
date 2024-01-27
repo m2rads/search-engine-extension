7e121740712e60dd36774377210dcf5dd8bfc85a
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-01-27 09:36:16 -0800

    add feature to send searchTerms to content-script

--- popup.js
+++ popup.js
@@ -4,7 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function handleSearch() {
         console.log('Searching for:', searchInput.value);
-        // TODO: Implement additional search functionality here if needed
+        let searchTerm = searchInput.value
+
+        // Send the search Input to content script
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "search", searchTerm: searchTerm}, function(response) {
+                if (response && !response.found) {
+                    console.log("Search term not found on this page.");
+                }
+            });
+        });
     }
 
     // Event listener for Enter key in the input field
