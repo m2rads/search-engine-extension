0d78c980edb89c1ab78e0b9a2b3c639435f76068
Mohammad Rad <mesmaeil@geeksquad.ca>
2024-01-27 18:39:07 -0800

    add some more stuff

--- content-script.js
+++ content-script.js
@@ -1,19 +1,9 @@
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 function searchAndMark(searchTerm) {
     let count = 0;
-    const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-    let node;
-
-    while (node = walker.nextNode()) {
-        if (node.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
-            const parent = node.parentNode;
-            const span = document.createElement('span');
-            const anchorId = `searchResult${count}`;
-            span.id = anchorId;
-            span.innerHTML = node.textContent.replace(new RegExp(searchTerm, 'gi'), match => `<mark>${match}</mark>`);
-            parent.replaceChild(span, node);
-            count++;
-        }
-    }
 
     return count;
 }
