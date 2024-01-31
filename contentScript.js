48e5f7aa1ed87a1699f9a4b56ede2ad227b6a126
m2rads <argo.mohrad@gmail.com>
2024-01-30 17:39:27 -0800

    update

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-(async () => {
+(() => {
 
     const searchResults = [
         {kind: 'customsearch#result', title: 'Learn Next.js: Streaming | Next.js', link: 'https://nextjs.org/learn/dashboard-app/streaming', displayLink: 'nextjs.org', cacheId: "XmJuNBpcjeMJ", htmlSnippet: "Built on top of <b>Suspense</b>, Loading UI allows you to create a fallback for specific route segments, and automatically stream content as it becomes ready.", },
